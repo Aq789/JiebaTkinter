1648c79c8e8610b4342d7837fbb89e5134b6b07e
Wang <wangjingqian123@outlook.com>
2026-03-23 22:11:59 +0800

    0.0.3 - 为分词结果创建预览表格 - 为词典创建预览表格 - 更改左侧窗口大小

--- app/widget/left_frame.py
+++ app/widget/left_frame.py
@@ -12,6 +12,64 @@ class LeftFrame:
         tab_word_seg_result = ttk.Frame(notebook) # 创建分词结果标签页
         tab_word_dic = ttk.Frame(notebook) # 创建字典标签页
 
+        tab_word_seg_result.grid_rowconfigure(0, weight=1)
+        tab_word_seg_result.grid_columnconfigure(0, weight=1)
+        tab_word_dic.grid_rowconfigure(0, weight=1)
+        tab_word_dic.grid_columnconfigure(0, weight=1)
+
         # 加入notebook标签页中
         notebook.add(tab_word_seg_result, text="分词结果")
         notebook.add(tab_word_dic, text="字典")
+
+        """分词结果"""
+        # 开始创建新表格
+        show_word_seg_result = ttk.Treeview(tab_word_seg_result, columns=("id", "word_name", "word_frequency", "word_class"), show="headings")
+
+        # 定义每一列的标题
+        show_word_seg_result.heading("id", text="序号")
+        show_word_seg_result.heading("word_name", text="词名")
+        show_word_seg_result.heading("word_frequency", text="词频")
+        show_word_seg_result.heading("word_class", text="词性")
+
+        # 定义每一列的宽度
+        show_word_seg_result.column("id", width=20)
+        show_word_seg_result.column("word_name", width=60)
+        show_word_seg_result.column("word_frequency", width=30)
+        show_word_seg_result.column("word_class", width=30)
+
+        show_word_seg_result.grid(row=0, column=0, sticky="nsew", pady=10, padx=10) # 放置表格
+
+        height_scroll_seg_result = tk.Scrollbar(tab_word_seg_result, orient="vertical", command=show_word_seg_result.yview) # 创建纵向滚动条
+        height_scroll_seg_result.grid(row=0, column=1, sticky="ns")
+        show_word_seg_result.config(yscrollcommand=height_scroll_seg_result.set)
+
+        edit_word_seg_result = ttk.Button(tab_word_seg_result, text="编辑分词结果")
+        edit_word_seg_result.grid(row=1, column=0, sticky="e", pady=5, padx=10)
+
+        start_word_seg_result = ttk.Button(tab_word_seg_result, text="开始分词")
+        start_word_seg_result.grid(row=1, column=0, sticky="w", pady=5, padx=10)
+
+        """词典"""
+        show_word_dic = ttk.Treeview(tab_word_dic, columns=("id", "word_name", "word_frequency", "word_class"), show="headings")
+
+        # 定义每一列的标题
+        show_word_dic.heading("id", text="序号")
+        show_word_dic.heading("word_name", text="词名")
+        show_word_dic.heading("word_frequency", text="词频")
+        show_word_dic.heading("word_class", text="词性")
+
+        # 定义每一列的宽度
+        show_word_dic.column("id", width=20)
+        show_word_dic.column("word_name", width=60)
+        show_word_dic.column("word_frequency", width=30)
+        show_word_dic.column("word_class", width=30)
+
+        show_word_dic.grid(row=0, column=0, sticky="nsew", pady=10, padx=10)  # 放置表格
+
+        height_scroll_word_dic = tk.Scrollbar(tab_word_dic, orient="vertical",
+                                                command=show_word_dic.yview)  # 创建纵向滚动条
+        height_scroll_word_dic.grid(row=0, column=1, sticky="ns")
+        show_word_dic.config(yscrollcommand=height_scroll_word_dic.set)
+
+        edit_word_dic = ttk.Button(tab_word_dic, text="编辑词典")
+        edit_word_dic.grid(row=1, column=0, sticky="e", pady=5, padx=10)
--- app/widget/paned_window.py
+++ app/widget/paned_window.py
@@ -12,5 +12,5 @@ class PanedWindow:
         self.new_left_frame = app.widget.left_frame.LeftFrame(self.new_paned_window) # 加载左侧模块
         self.new_right_frame = app.widget.right_frame.RightFrame(self.new_paned_window) # 加载右侧模块
 
-        self.new_paned_window.add(self.new_left_frame.left_frame, minsize=20, width=200) # 放置左侧内容
+        self.new_paned_window.add(self.new_left_frame.left_frame, minsize=20, width=240) # 放置左侧内容
         self.new_paned_window.add(self.new_right_frame.right_frame, minsize=50) # 放置右侧内容
